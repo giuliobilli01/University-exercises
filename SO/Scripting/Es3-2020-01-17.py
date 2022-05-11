8ec314f8cb44e2ed04ce2c168b6dd76b6964a467
Giulio Billi <giulio.billi2@studio.unibo.it>
2022-05-12 00:03:17 +0200

    Fixed

--- SO/Scripting/Es3-2020-01-17.py
+++ SO/Scripting/Es3-2020-01-17.py
@@ -9,29 +9,18 @@ import sys
 import subprocess
 
 def main():
-  commands = []
-  string = []
+
+  string = ''
 
   for i in range(1, len(sys.argv), 1):
     if sys.argv[i] != '//':
-        string.append(sys.argv[i])
+        string += ' ' + sys.argv[i]
     else:
-      commands.append(string[0:])
-      string.clear()
-  commands.append(string[0:])
-  
-  
+      string += ' &'
 
-  processes = []
-
-  for command in commands:
-    process = subprocess.Popen(command, shell=True)
-    processes.append(process)  
-
-  for p in processes:
-    output = p.wait()
+  print(string)
   
-  print(output)
+  os.system(f'{string}')
 
 if __name__ == '__main__':
   main()
