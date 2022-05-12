4e385ea21192f07dbd833a5439e36a424d3f721e
Giulio Billi <giulio.billi2@studio.unibo.it>
2022-05-12 21:26:07 +0200

    Added Es3-2020-02-21 in python

--- SO/Scripting/Python/Es3-2020-02-21.py
+++ SO/Scripting/Python/Es3-2020-02-21.py
@@ -8,3 +8,31 @@
 import os
 import sys
 
+def main():
+  files = {}
+  dir = ''
+  if len(sys.argv) > 1:
+    dir = sys.argv[1]
+  else:
+    dir = os.getcwd()
+  
+  for file in os.listdir(dir):
+    fileName, fileExt = os.path.splitext(file)
+    files.setdefault(fileExt, [])
+    files[fileExt].append(fileName)
+
+  sortedExt = sorted(files.keys())
+
+  print("I file della directory in ordine di estensione sono:")
+  for ext in sortedExt:
+    print(f'{files[ext]}{ext}\n')    
+
+
+
+if __name__ == "__main__":
+  main()
+
+
+
+
+
