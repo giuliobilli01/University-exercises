100c394b1782384772585d05cfb0e94c4559d9c1
Giulio Billi <giulio.billi2@studio.unibo.it>
2022-06-14 15:39:30 +0200

    Added Es2 bootstrap

---
+++ Web-Tecnologies/Exercises/Bootstrap/Es2/script.js
@@ -0,0 +1,30 @@
+function showOnClick1() {
+  const div1=document.getElementById('div1');
+
+
+  if (div1.style.display == 'block') {
+      div1.style.display= "none";
+    }else {
+      div1.style.display="block";
+    }
+}
+
+function showOnClick2() {
+  const div2=document.getElementById('div2');
+
+  if (div2.style.display == 'block') {
+    div2.style.display= "none";
+  }else {
+    div2.style.display="block";
+  }
+}
+
+function showOnClick3() {
+  const div3=document.getElementById('div3');
+
+  if (div3.style.display == 'block') {
+    div3.style.display= "none";
+  }else {
+    div3.style.display="block";
+  }
+}
